d65619fb90d4d1e62c1b6a9e89370ab9e27f7db8
Pukaliny01 <ypukaliny@gmail.com>
2025-11-02 14:15:16 +0530

    Finished Navbar with menu logic and a little  dot on basket

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -4,16 +4,16 @@ import {assets} from '../../assets/assets.js'
 
 const NavBar = () => {
 
-  const [menu,setMenu] = useState("contact us");
+  const [menu,setMenu] = useState("contact-us");
 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="Logo" className="logo" />
       <ul className="navbar-menu">
-        <li className={menu==="home"?"active":""}>home</li>
-        <li className={menu==="menu"?"active":""}>menu</li>
-        <li className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li className={menu==="contact us"?"active":""}>contact us</li>
+        <li onClick={() => setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+        <li onClick={() => setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        <li onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-app</li>
+        <li onClick={() => setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
