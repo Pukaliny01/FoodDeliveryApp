9d58e2c9a0a2f060a72e18f37fa7506f23cc049d
Pukaliny01 <ypukaliny@gmail.com>
2025-11-02 10:34:45 +0530

    Organized Navbar files into folder and added logo image into the website

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './NavBar.css'
+import {assets} from '../../assets/assets.js'
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-
+        <img src={assets.logo} alt="Logo"/>
     </div>
   )
 }
