234803083a51c3cf32bfb9e89dca0fed6d40d9ac
Pukaliny01 <ypukaliny@gmail.com>
2025-11-02 20:27:33 +0530

    Set up the Routes for navigating pages without refreshing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar.jsx'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 
 const App = () => {
   return (
     <div className='app'>
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
