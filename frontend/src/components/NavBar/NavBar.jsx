afd0593fbe1ad76be681c7a954f2e7f78b3b3342
Pukaliny01 <ypukaliny@gmail.com>
2025-11-08 21:19:54 +0530

    Create LogIn PopUp

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import './NavBar.css'
 import {assets} from '../../assets/assets.js'
 import { Link } from 'react-router-dom'
 
-const NavBar = () => {
+const NavBar = ({setShowLogin}) => {
 
   const [menu,setMenu] = useState("contact-us");
 
@@ -22,7 +22,7 @@ const NavBar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>Sign In</button>
+        <button onClick={()=>setShowLogin(true)}>Sign In</button>
       </div>
     </div>
   )
