da11d2ed6eb784e42c963ade4c334f077bab1639
Pukaliny01 <ypukaliny@gmail.com>
2025-11-15 16:09:28 +0530

    Fix static image serving and update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // app config
 const app = express();
@@ -12,17 +18,22 @@ const port = 4000;
 app.use(cors());
 app.use(express.json());
 
-//DB connection
+// DB connection
 connectDB();
 
-//api endpoints
+// static files
+app.use("/images", express.static(path.join(__dirname, "uploads")));
+
+// api routes
 app.use("/api/food", foodRouter);
 
 app.get('/', (req, res) => {
   res.send('API Working');
-})
+});
 
 app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`); 
-})
+  console.log(`Server started on http://localhost:${port}`);
+  console.log("Static path:", path.join(__dirname, 'uploads'));
+});
+
 
